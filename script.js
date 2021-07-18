8b0d38a102f6656de2772d213898323d85357f37
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-07-18 13:07:55 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const loginInput = document.querySelector("#login_form input");
+const loginButton = document.querySelector("#login_form button");
+
+
+function handleLoginBtnClick() {
+    console.log(loginInput.value);
+}
+
+loginButton.addEventListener("click",handleLoginBtnClick);
