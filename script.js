dfca15b3f8048ebf4c756be713b527a23e0ffc3b
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-07-19 23:20:16 +0900

    Revert "Login : Saving Username"
    This reverts commit c870c5c075ebb974fdcd1490ba521cff7a7190f6.

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const loginForm = document.querySelector("#login_form");
 const loginInput = document.querySelector("#login_form input");
-const greeting = document.querySelector("#greeting")
-
-const HIDDEN_CLASSNAME = "hidden"
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add("hidden");
-    const username = loginInput.value;
-    greeting.innerText = `Hello ${loginInput.value}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    console.log(loginInput.value);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
