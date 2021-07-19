1393ff47a06839342de6baea1cd2346012972291
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-07-19 10:32:00 +0900

    Form Submission

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # To-Do-List
  
+* 바닐라JS를 통한 To-Do-List 만들기
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const loginButton = document.querySelector("#login_form button");
 
 
 function handleLoginBtnClick() {
-    console.log(loginInput.value);
-}
+    const username = loginInput.value;
+    console.log = (username);
 
 loginButton.addEventListener("click",handleLoginBtnClick);
