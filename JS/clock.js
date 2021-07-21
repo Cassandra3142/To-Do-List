f131cf1379d085e2a7e4afe13099bb46621ec3da
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-07-21 10:05:48 +0900

    Clock : PadStart

--- JS/clock.js
+++ JS/clock.js
@@ -4,7 +4,11 @@ clock.innerText = "kkk" ;
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const secounds = String(date.getSeconds()).padStart(2,"0");
+
+    clock.innerText = `${hours} : ${minutes} : ${secounds}`;
 }
 
 getClock();
