93afc06c8a4eed3681efb83cf78a1f00911ab2e1
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-07-24 09:53:22 +0900

    Weather : Weather API

--- Clone/JS/background.js
+++ Clone/JS/background.js
@@ -4,6 +4,6 @@ const chosenImages = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImages}`;
+bgImage.src = `./Clone/img/${chosenImages}`;
 
 document.body.appendChild(bgImage);
