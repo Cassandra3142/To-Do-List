b1bda26e7d8423884fb67540f8793382dced7be6
Cassandra3142 <85780707+Cassandra3142@users.noreply.github.com>
2021-08-08 16:17:19 +0900

    다시만들기

--- Clone/JS/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.webp","1.webp","2.webp"];
-
-const chosenImages = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `./Clone/img/${chosenImages}`;
-
-document.body.appendChild(bgImage);
--- README.md
+++
@@ -1,3 +0,0 @@
-# To-Do-List
- 
-* 바닐라JS를 통한 To-Do-List 만들기
